1993be9c109d4ad1b18579ca19c75cfe20b136d1
Dilip Agheda <dilip_agheda@yahoo.com>
2020-04-01 12:35:39 +1100

    Setup App using a standard MVC template, created Data models and initial migration using Entity Framework

---
+++ MovieWatchList/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieWatchList.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<UserMovie> UserMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<UserMovie>().HasKey(m => new { m.MovieId, m.UserId });
+        }
+    }
+}
---
+++ MovieWatchList/Data/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieWatchList.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            this.Watchlist = new HashSet<UserMovie>();
+        }
+        public string FirstName { get; set; }
+        public virtual ICollection<UserMovie> Watchlist { get; set; }
+    }
+}
